57c06ed829586ec6cecf59b8c251440dd0145ba5
snowindH <2824216045@qq.com>
2018-05-17 11:09:41 +0800

    add post

--- HelloWorld/tool/views.py
+++ HelloWorld/tool/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello,world,You're at the tool index.")
+#def index(request):
+#    return HttpResponse("Hello,world,You're at the tool index.")
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count,post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+    return HttpResponse(post_lists)
